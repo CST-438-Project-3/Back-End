8ebb053976644f883cba653a1ea71b02bae382ee
spineda12 <spinedajimenez@csumb.edu>
2024-11-12 16:51:11 -0800

    fixed connection to mysql and works on docker and deploys on google cloud @ https://project3-service-701122319569.us-central1.run.app

--- project3/src/test/java/com/cst438/project3/Project3ApplicationTests.java
+++ project3/src/test/java/com/cst438/project3/Project3ApplicationTests.java
@@ -2,11 +2,12 @@ package com.cst438.project3;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class Project3ApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
 }
