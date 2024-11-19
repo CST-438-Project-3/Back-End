a6fea8288de046c0d15514290dd4c14c30a2dee3
ashley <132930723+xeahh@users.noreply.github.com>
2024-11-19 14:34:23 -0800

    gradle files and .gitignore

---
+++ src/test/java/group15/pantrypal/PantrypalApplicationTests.java
@@ -0,0 +1,13 @@
+package group15.pantrypal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PantrypalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
